107c85793d0944c3d0ea225d7e00d6141bc51239
Trevor <trevor.a.bauer@dhhs.nh.gov>
2022-07-25 17:25:45 -0400

    Initial Commit

---
+++ rw_visual.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+# Keep making new walks, as long as the program is active. 
+while True:
+    # Make a new random walk, and plot the points.
+    rw=RandomWalk(500000)
+    rw.fill_walk()
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+    # Emphasize the first and last points
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    plt.show()
+
+    keep_running = input("Make another walk (y/n)?: ").lower()
+    if not keep_running.startswith('y'):
+        break
