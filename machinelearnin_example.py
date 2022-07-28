d713d87aa823d11d7a105358a27364d14c801a82
Trevor <trevor.a.bauer@dhhs.nh.gov>
2022-07-27 22:28:12 -0400

    minor updates

--- machinelearnin_example.py
+++ machinelearnin_example.py
@@ -1,33 +1,29 @@
-
 #Pandas for data analysis
 import pandas as pd
 
 #Sklearn for modeling
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.externals import joblib
-=======
+import joblib
 
->>>>>>> 31db6185e2a2690babf134a92575561d129b68bc
 #Import data
 music_data = pd.read_csv('music.csv')
-
-#Set X to the data, minus the genre (since we want to predict this)
 X = music_data.drop(columns=['genre'])
-
-#Set y to the dependent variable
 y = music_data['genre']
 
-<<<<<<< HEAD
 #Create a test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
 #Create the model 
 model = DecisionTreeClassifier()
-#fit the model
-model.fit(X_train,y_train)
+model.fit(X.values,y.values)
 
-#make a couple hardcoded predictions.
-predictions = model.predict(X_test)
+# model = joblib.dump(model, 'music-recommender.joblib')
+# model = joblib.load('music-recommender.joblib')
+#fit the model
+#model.fit(X_train,y_train)
+
+#make a hardcoded prediction, 21 year old male.. should like "Hip-Hop", check that array states this.
+predictions = model.predict([[21,1]])
 
 
 #general rule of thumb ~70-80% of the data for training, and the rest for testing.
@@ -44,6 +40,3 @@ model.fit(X,y)
 
 #make a couple hardcoded predictions.
 model.predict([[21,1], [22,0]])
-
-
->>>>>>> 31db6185e2a2690babf134a92575561d129b68bc
