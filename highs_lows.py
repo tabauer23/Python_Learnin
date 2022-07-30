91846fad4e357671b087b32398974a32a8142b49
Trevor Bauer <bauer.trevor@gmail.com>
2022-07-30 14:47:47 -0400

    Added lines

--- highs_lows.py
+++ highs_lows.py
@@ -20,5 +20,7 @@ with open(filename) as f:
 with open(filename) as f:
   reader = csv.reader(f)
   header_row = next(reader)
-  for index, column_header in enumerate(header_row):
-    print(index, column_header)
+  highs = []
+  for row in reader:
+    highs.append(row[1])
+  print(highs)
